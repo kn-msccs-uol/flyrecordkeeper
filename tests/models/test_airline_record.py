a2e042c7a1fced5c5807b203d59eb5ce4d91cb60
Darryl Daniels <d.k.daniels@liverpool.ac.uk>
2025-03-24 18:50:27 +0200

    BugFix: Airline unit tests
    Fixed test issues on airline record

--- tests/models/test_airline_record.py
+++ tests/models/test_airline_record.py
@@ -11,21 +11,21 @@ class TestAirlineRecord(unittest.TestCase):
     #Set up of sample data
     def setUp(self):
         self.sample_data = {
-            "record_id": 1,
+            "id": 1,
             "company_name": "British Airways"
         }
-        self.airline = AirlineRecord(self.sample_data)
+        self.airline = AirlineRecord(self.sample_data["id"], self.sample_data["company_name"])
 
     #Test coversion to dictionary type
     def test_to_dict(self):
         airline_dict = self.airline.to_dict()
-        self.assertEqual(airline_dict["id"], self.sample_data["record_id"])
+        self.assertEqual(airline_dict["id"], self.sample_data["id"])
         self.assertEqual(airline_dict["company_name"], self.sample_data["company_name"])
 
     #Test creation from dictionary type
     def test_from_dict(self):
         airline = AirlineRecord.from_dict(self.sample_data)
-        self.assertEqual(airline.record_id, self.sample_data["record_id"])
+        self.assertEqual(airline.id, self.sample_data["id"])
         self.assertEqual(airline.company_name, self.sample_data["company_name"])
 
     #Test validation of sample data
